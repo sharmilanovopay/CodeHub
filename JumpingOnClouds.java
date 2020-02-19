80926b0f2003c76141fad63be49213ffe982f30a
DSharmila1710 <56862689+DSharmila1710@users.noreply.github.com>
2020-02-19 13:05:33 +0530

    Program to count the steps while jumping on the clouds

---
+++ JumpingOnClouds.java
@@ -0,0 +1,63 @@
+package HackerRank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class JumpingOnClouds {
+
+    // Complete the jumpingOnClouds function below.
+    static int jumpingOnClouds(int[] c) {
+        int steps = 0;
+        int i=0;
+        while(i<c.length){
+            if(c[i]==0){
+                if( (i+2)<c.length && c[i+2]==0 ){
+                    steps++;
+                    i+=2;
+                }else if( (i+1)<c.length && c[i+1]==0 ){
+                    steps++;
+                    i+=1;
+                }else{
+                    break;
+                }
+            }else{
+                i++;
+            }
+        }
+        return steps;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] c = new int[n];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int result = jumpingOnClouds(c);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
